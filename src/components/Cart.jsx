ec3314d094541fe7ebc5440c3a1cfea41e11d1a1
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-07-02 20:24:23 +0530

    new

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,42 +1,28 @@
 import React from 'react'
 import "../styles/cart.scss"
 import {AiFillDelete} from "react-icons/ai"
+import { useSelector } from 'react-redux';
 
 const img1 =
   "https://www.reliancedigital.in/medias/Apple-MGN63HNA-Laptops-491946461-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxNzczNDJ8aW1hZ2UvanBlZ3xpbWFnZXMvaDVhL2gyZC85NDQzMDgzNTgzNTE4LmpwZ3xhYzRiNWIxZGQ2NjNiNWIyYjI0Y2ZkYTZlZWQ3MTFjZTMxYzVmNDBiNmM5Mzk5OTM2OGVkZmExMjMyYjIxNDQ4";
-const img2 =
-  "https://cdn.shopify.com/s/files/1/2428/5565/products/Neemans-HaleBlack-ReLive-Knits-Jogger-FrontRightLogo-Comfortable-Shoes_1024x.jpg?v=1662876260";
+// const img2 =
+//   "https://cdn.shopify.com/s/files/1/2428/5565/products/Neemans-HaleBlack-ReLive-Knits-Jogger-FrontRightLogo-Comfortable-Shoes_1024x.jpg?v=1662876260";
 
 
 
 const Cart = () => {
+    const {cartItems} = useSelector((state)=> state.cart)
   return (
     <div className="cart">
         <main>
-            <CartItem
-            imgSrc={img1}
-            name={"Mac Book"}
-            price={56232}
-            qty={1}
-            id="random"/>
-             <CartItem
-            imgSrc={img1}
-            name={"Mac Book"}
-            price={56232}
-            qty={1}
-            id="random"/>
-             <CartItem
-            imgSrc={img1}
-            name={"Mac Book"}
-            price={56232}
-            qty={1}
-            id="random"/>
-             <CartItem
-            imgSrc={img1}
-            name={"Mac Book"}
-            price={56232}
-            qty={1}
-            id="random"/>
+           
+             
+                {cartItems.length > 0 ?(
+                    cartItems.map(i=>(
+                        <CartItem imgSrc={img1} name={"mac book"} price={232423} qty={1} id="sfdsdf"/>
+                    ))
+                ):( <h1>No items</h1>)}
+             
         </main>
         <aside>
             <h2>Subtotal: ${2000}</h2>
