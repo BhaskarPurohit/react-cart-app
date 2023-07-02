c2a9076b1403b54ddc7765ce676fe3efcbfa689b
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-07-02 20:24:15 +0530

    new

---
+++ src/redux/reducer.jsx
@@ -0,0 +1,8 @@
+import { createReducer } from "@reduxjs/toolkit";
+
+export const cartReducer = createReducer({
+    cartItems:[], subTotal:0, shipping:0, tax:0, total:0,
+},{addToCart: (state, action)=>{
+    const item = action.payload;
+    
+}})
